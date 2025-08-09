bb9ad7e8c3d65f3a64450e0dd2f224eb2d2a5c66
pengjian <pengjian.cool@bytedance.com>
2025-08-10 00:18:39 +0800

    simple agent with tool2

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ load_dotenv(dotenv_path=Path(".env"), verbose=True)
 from agent.react_agent import create_agent
 
 def query(question: str):
-    pass
+    react_agent = create_agent()
+    result = react_agent.invoke(
+        {
+            "messages": [
+                {"role": "user", "content": question}
+            ]
+        }
+    )
+    print(result)
+
 
 
 
 if __name__ == "__main__":
-    # question = "What is the capital of France?"
-    # query(question)
-
-    react_agent = create_agent()
-    print(react_agent.invoke(
-        {
-            "messages": [
-                {"role": "user", "content": "what is the weather in sf"}
-            ]
-        }
-    ))
+    question = "what is the weather in sf?"
+    query(question)
